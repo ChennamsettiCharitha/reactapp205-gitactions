531d642d649b17c57d40d8615f2d5154125f018a
Charitha <charithachennamsetty@gmail.com>
2025-07-29 23:44:39 +0530

    Updated style.css also

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import './style.css';
 function App() {
   return (
     <div className="container">
-      <h1>Welcome to My React Project - S205</h1><p>
-        CI/CD AND Cloud DevOps
-      </p>
+      <h1>Welcome to My React Project - S205</h1>
+      <p>CI/CD and Cloud DevOps</p>
       <p>This is a simple homepage deployed on GitHub Actions using Vite.</p>
       <p>
-        Done With Git Pages & Actions and all.......
+        <span className="highlight-blue">GitHub Pages</span> + 
+        <span className="highlight-green"> GitHub Actions</span> = 
+        <span className="highlight-orange"> Success!</span>
       </p>
     </div>
   );
