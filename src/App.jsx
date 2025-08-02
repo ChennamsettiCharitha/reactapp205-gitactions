2f91111e2d8097cf6a012954402b5115a56eb37b
Charitha <charithachennamsetty@gmail.com>
2025-08-02 10:09:39 +0530

    Updated style.css

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,7 @@ function App() {
       <p>CI/CD and Cloud DevOps</p>
       <p>This is a simple homepage deployed on GitHub Actions using Vite.</p>
       <p>
-        <span className="highlight-blue">GitHub Pages</span> + 
-        <span className="highlight-green"> GitHub Actions</span> = 
-        <span className="highlight-orange"> Success!</span>
+        <span className="highlight">Done with Git Pages & Git Actions</span>
       </p>
     </div>
   );
